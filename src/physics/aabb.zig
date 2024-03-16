e81cf4409e8fab97dc3ea0371cd6d733d12fb094
Stefan Partheymüller <s.partheymueller@initics.de>
2024-03-16 18:10:58 +0100

    refactoring

--- src/physics/aabb.zig
+++ src/physics/aabb.zig
@@ -6,52 +6,84 @@ pub const Aabb = struct {
     y: f32,
     w: f32,
     h: f32,
-    vx: f32,
-    vy: f32,
+    velocityX: f32,
+    velocityY: f32,
 };
 
 pub const AabbSweepResult = struct {
-    normalx: f32 = 0,
-    normaly: f32 = 0,
+    normalX: f32 = 0,
+    normalY: f32 = 0,
     time: f32 = 1,
     remainingTime: f32 = 0,
 
     pub fn assign(self: *AabbSweepResult, other: AabbSweepResult) void {
         self.time = other.time;
         self.remainingTime = other.remainingTime;
-        self.normalx = other.normalx;
-        self.normaly = other.normaly;
+        self.normalX = other.normalX;
+        self.normalY = other.normalY;
     }
 };
 
-pub fn createAabb(x: f32, y: f32, w: f32, h: f32, vx: f32, vy: f32) Aabb {
-    return Aabb{ .x = x, .y = y, .w = w, .h = h, .vx = vx, .vy = vy };
+pub fn createAabb(
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    velocityX: f32,
+    velocityY: f32,
+) Aabb {
+    return Aabb{
+        .x = x,
+        .y = y,
+        .w = width,
+        .h = height,
+        .velocityX = velocityX,
+        .velocityY = velocityY,
+    };
 }
 
-pub fn createBroadphaseAabb(x: f32, y: f32, w: f32, h: f32, vx: f32, vy: f32) Aabb {
-    const pos_x = if (vx > 0) x else x + vx;
-    const pos_y = if (vy > 0) y else y + vy;
-    const width = if (vx > 0) vx + w else w - vx;
-    const height = if (vy > 0) vy + h else h - vy;
-    return Aabb{ .x = pos_x, .y = pos_y, .w = width, .h = height, .vx = vx, .vy = vy };
+pub fn createBroadphaseAabb(
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    velocityX: f32,
+    velocityY: f32,
+) Aabb {
+    const targetX = if (velocityX > 0) x else x + velocityX;
+    const targetY = if (velocityY > 0) y else y + velocityY;
+    const targetWidth = if (velocityX > 0) velocityX + width else width - velocityX;
+    const targetHeight = if (velocityY > 0) velocityY + height else height - velocityY;
+    return Aabb{
+        .x = targetX,
+        .y = targetY,
+        .w = targetWidth,
+        .h = targetHeight,
+        .velocityX = velocityX,
+        .velocityY = velocityY,
+    };
 }
 
+/// Performs a basic AABB collision detection.
+/// This is mainly used in broadphase collision detection.
 pub fn check(a: Aabb, b: Aabb) bool {
-    return a.x < b.x + b.w and // left edge is past right edge of obstacle?
-        a.x + a.w > b.x and // right edge is past left edge of obstacle?
-        a.y < b.y + b.h and // top edge is past bottom edge of obstacle?
-        a.y + a.h > b.y; // bottom edge is past top edge of obstacle?
+    return a.x < b.x + b.w and // Left edge is within right side of the collider?
+        a.x + a.w > b.x and // Right edge is within left side of the collider?
+        a.y < b.y + b.h and // Top edge is within bottom side of the collider?
+        a.y + a.h > b.y; // Bottom edge is within top side of the collider?
 
 }
 
+/// Performs an AABB sweep.
 pub fn sweep(a: Aabb, b: Aabb) AabbSweepResult {
     var xInvEntry: f32 = undefined;
     var xInvExit: f32 = undefined;
     var yInvEntry: f32 = undefined;
     var yInvExit: f32 = undefined;
 
-    // find the distance between the objects on the near and far sides for both x and y
-    if (a.vx > 0.0) {
+    // Find the distance between the objects on the near and far sides for both
+    // x and y.
+    if (a.velocityX > 0) {
         xInvEntry = b.x - (a.x + a.w);
         xInvExit = (b.x + b.w) - a.x;
     } else {
@@ -59,7 +91,7 @@ pub fn sweep(a: Aabb, b: Aabb) AabbSweepResult {
         xInvExit = b.x - (a.x + a.w);
     }
 
-    if (a.vy > 0.0) {
+    if (a.velocityY > 0) {
         yInvEntry = b.y - (a.y + a.h);
         yInvExit = (b.y + b.h) - a.y;
     } else {
@@ -67,93 +99,96 @@ pub fn sweep(a: Aabb, b: Aabb) AabbSweepResult {
         yInvExit = b.y - (a.y + a.h);
     }
 
-    // find time of collision and time of leaving for each axis (if statement is to prevent divide by zero)
-    var xEntry: f32 = undefined;
-    var xExit: f32 = undefined;
-    var yEntry: f32 = undefined;
-    var yExit: f32 = undefined;
+    // Calculate time of collision and time of leaving for each axis
+    // (if statements are to prevent division by zero).
+    var entryX: f32 = undefined;
+    var exitX: f32 = undefined;
+    var entryY: f32 = undefined;
+    var exitY: f32 = undefined;
 
-    if (a.vx == 0.0) {
-        xEntry = -math.inf(f32);
-        xExit = math.inf(f32);
+    if (a.velocityX == 0) {
+        entryX = -math.inf(f32);
+        exitX = math.inf(f32);
     } else {
-        xEntry = xInvEntry / a.vx;
-        xExit = xInvExit / a.vx;
+        entryX = xInvEntry / a.velocityX;
+        exitX = xInvExit / a.velocityX;
     }
 
-    if (a.vy == 0.0) {
-        yEntry = -math.inf(f32);
-        yExit = math.inf(f32);
+    if (a.velocityY == 0) {
+        entryY = -math.inf(f32);
+        exitY = math.inf(f32);
     } else {
-        yEntry = yInvEntry / a.vy;
-        yExit = yInvExit / a.vy;
+        entryY = yInvEntry / a.velocityY;
+        exitY = yInvExit / a.velocityY;
     }
 
-    // find the earliest/latest times of collision
-    const entryTime: f32 = @max(xEntry, yEntry);
-    const exitTime: f32 = @min(xExit, yExit);
+    // Get earliest/latest times of collision.
+    const entryTime: f32 = @max(entryX, entryY);
+    const exitTime: f32 = @min(exitX, exitY);
 
-    if (entryTime > exitTime or (xEntry < 0.0 and yEntry < 0.0) or xEntry > 1.0 or yEntry > 1.0) {
-        // if there was no collision
+    // Check if there was no collision.
+    if (entryTime > exitTime or (entryX < 0 and entryY < 0) or entryX > 1 or entryY > 1) {
         return .{
-            .normalx = 0,
-            .normaly = 0,
+            .normalX = 0,
+            .normalY = 0,
             .time = 1,
             .remainingTime = 0,
         };
-    } else {
-        // if there was a collision
-        var normalx: f32 = undefined;
-        var normaly: f32 = undefined;
+    }
+    // If there was a collision.
+    else {
+        var normalX: f32 = undefined;
+        var normalY: f32 = undefined;
         // calculate normal of collided surface
-        if (xEntry > yEntry) {
-            if (xInvEntry < 0.0) {
-                normalx = 1.0;
-                normaly = 0.0;
+        if (entryX > entryY) {
+            if (xInvEntry < 0) {
+                normalX = 1;
+                normalY = 0;
             } else {
-                normalx = -1.0;
-                normaly = 0.0;
+                normalX = -1;
+                normalY = 0;
             }
         } else {
-            if (yInvEntry < 0.0) {
-                normalx = 0.0;
-                normaly = 1.0;
+            if (yInvEntry < 0) {
+                normalX = 0;
+                normalY = 1;
             } else {
-                normalx = 0.0;
-                normaly = -1.0;
+                normalX = 0;
+                normalY = -1;
             }
         }
 
         // return the time of collision
         return .{
-            .normalx = normalx,
-            .normaly = normaly,
+            .normalX = normalX,
+            .normalY = normalY,
             .time = entryTime,
-            .remainingTime = 1.0 - entryTime,
+            .remainingTime = 1 - entryTime,
         };
     }
 }
 
+/// Respond to AABB collision by sliding the entity along the edge of the
+/// collider.
 pub fn responseSlide(velocity: Velocity, sweepResult: AabbSweepResult) Velocity {
-    // Respond to collision by sliding the entity along the
-    // edge of the collider.
     const dotprod =
-        (velocity.x * sweepResult.normaly +
-        velocity.y * sweepResult.normalx) *
+        (velocity.x * sweepResult.normalY +
+        velocity.y * sweepResult.normalX) *
         sweepResult.remainingTime;
 
     return .{
-        .y = dotprod * sweepResult.normalx,
-        .x = dotprod * sweepResult.normaly,
+        .y = dotprod * sweepResult.normalX,
+        .x = dotprod * sweepResult.normalY,
     };
 }
 
-/// Push entity along the collision edge.
+/// Respond to AABB collision by pushing the entity along the edge of the
+/// collider.
 pub fn responsePush(velocity: Velocity, sweepResult: AabbSweepResult) Velocity {
     const magnitude =
         math.sqrt((velocity.x * velocity.x + velocity.y * velocity.y)) *
         sweepResult.remainingTime;
-    var dotprod = velocity.x * sweepResult.normaly + velocity.y * sweepResult.normalx;
+    var dotprod = velocity.x * sweepResult.normalY + velocity.y * sweepResult.normalX;
 
     if (dotprod > 0) {
         dotprod = 1;
@@ -162,20 +197,21 @@ pub fn responsePush(velocity: Velocity, sweepResult: AabbSweepResult) Velocity {
     }
 
     return .{
-        .x = dotprod * sweepResult.normaly * magnitude,
-        .y = dotprod * sweepResult.normalx * magnitude,
+        .x = dotprod * sweepResult.normalY * magnitude,
+        .y = dotprod * sweepResult.normalX * magnitude,
     };
 }
 
-// Bounce
+/// Respond to AABB collision by bouncing the entity off the edge of the
+/// collider.
 pub fn responseBounce(velocity: Velocity, sweepResult: AabbSweepResult) Velocity {
     var result: Velocity = .{
         .x = velocity.x * sweepResult.remainingTime,
         .y = velocity.y * sweepResult.remainingTime,
     };
 
-    if (@abs(sweepResult.normalx) > 0.0001) result.x = -result.x;
-    if (@abs(sweepResult.normaly) > 0.0001) result.y = -result.y;
+    if (@abs(sweepResult.normalX) > 0.0001) result.x = -result.x;
+    if (@abs(sweepResult.normalY) > 0.0001) result.y = -result.y;
 
     return result;
 }
